a84062f9318b85dcc07fabee53989525fc2a9b13
Vicky Jha <vickyjha256@gmail.com>
2024-02-21 20:39:05 +0530

    CORS ADDED in Controller

--- src/main/java/project/movieapp/movies/ReviewController.java
+++ src/main/java/project/movieapp/movies/ReviewController.java
@@ -3,13 +3,11 @@ package project.movieapp.movies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/route/reviews")
 public class ReviewController {
